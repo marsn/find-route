f6aae076cd2ba8c04594318396d07501f2948588
Marcel Novy <marsn@users.noreply.github.com>
2016-08-16 03:56:15 +0800

    Create findRoute.go

---
+++ findRoute.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type Road struct {
+	Departure string
+	Arrival   string
+	Price     int
+}
+
+type Route struct {
+	Roads []Road
+	Price int
+}
+
+var best_route Route
+
+func main() {
+	fmt.Println(FindRoute("A", "B", []Road{
+		{"A", "B", 3},
+		{"B", "C", 4},
+		{"A", "E", 2},
+		{"E", "B", 5},
+		{"D", "B", 5},
+	}, Route{}))
+}
+
+func FindRoute(departure string, arrival string, roads []Road, route Route) Route {
+	for _, road := range roads {
+		if road.Departure == departure {
+			if road.Arrival == arrival {
+				if road.Price += route.Price; best_route.Price == 0 || best_route.Price > road.Price {
+					best_route = Route{append(route.Roads, road), road.Price}
+				}
+			} else {
+				FindRoute(road.Arrival, arrival, roads, Route{append(route.Roads, road), route.Price + road.Price})
+			}
+		}
+	}
+	return best_route
+}
